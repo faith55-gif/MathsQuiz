dee06d83c3ee88238efa3f5623acf5687c409f56
PC <PC@DESKTOP-70M5C07>
2021-06-23 10:01:59 +0200

    Add project files.

---
+++ MathsQuiz/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace MathsQuiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.labelQuestion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MathsQuiz.Properties.Resources.Q5;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(638, 239);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(27, 371);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(236, 65);
+            this.button5.TabIndex = 1;
+            this.button5.Tag = "1";
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(27, 450);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(236, 65);
+            this.button6.TabIndex = 2;
+            this.button6.Tag = "3";
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(369, 371);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(261, 65);
+            this.button7.TabIndex = 3;
+            this.button7.Tag = "2";
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(369, 450);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(261, 65);
+            this.button8.TabIndex = 4;
+            this.button8.Tag = "4";
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelQuestion.Location = new System.Drawing.Point(1, 267);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(638, 70);
+            this.labelQuestion.TabIndex = 5;
+            this.labelQuestion.Text = "Question";
+            this.labelQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(641, 527);
+            this.Controls.Add(this.labelQuestion);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label labelQuestion;
+
+        public Form1(IContainer components, PictureBox pictureBox1, Button button5, Button button6, Button button7, Button button8, Label labelQuestion, EventHandler checkAnswerEvent, EventHandler labelQuestion_Click, int correctAnswer, int questionNumber, int score, int percentage, int totalQuestions)
+        {
+            this.components = components;
+            this.pictureBox1 = pictureBox1;
+            this.button5 = button5;
+            this.button6 = button6;
+            this.button7 = button7;
+            this.button8 = button8;
+            this.labelQuestion = labelQuestion;
+            CheckAnswerEvent = checkAnswerEvent;
+            this.labelQuestion_Click = labelQuestion_Click;
+            this.correctAnswer = correctAnswer;
+            this.questionNumber = questionNumber;
+            this.score = score;
+            this.percentage = percentage;
+            this.totalQuestions = totalQuestions;
+        }
+
+        public EventHandler CheckAnswerEvent { get; private set; }
+        public EventHandler labelQuestion_Click { get; private set; }
+    }
+}
+
---
+++ MathsQuiz/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathsQuiz
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        // quiz game variables
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestions(questionNumber);
+
+            totalQuestions = 5;
+
+
+        }
+
+
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+                // work out the percentage
+
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Quiz Ended!" + Environment.NewLine +
+                    "You have answered " + score + " questions correctly." + Environment.NewLine +
+                    "Your total percentage is " + percentage + "%" + Environment.NewLine +
+                    "Click OK to play again"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestions(questionNumber);
+
+            }
+
+            questionNumber++;
+            askQuestions(questionNumber);
+
+        }
+
+        private void askQuestions(int qnum)
+        {
+
+
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.Q1;
+
+                    labelQuestion.Text = "A clock strikes once at 1 o’clock, twice at 2 o’clock, thrice at 3 o’clock and so on. How many times will it strike in 24 hours?";
+
+                    button5.Text = "78";
+                    button6.Text = "135";
+                    button7.Text = "156";
+                    button8.Text = "196";
+
+                    correctAnswer = 3;
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.Q2;
+
+                    labelQuestion.Text = "Which of the following numbers gives 240 when added to its own square?";
+
+                    button5.Text = "15";
+                    button6.Text = "16";
+                    button7.Text = "18";
+                    button8.Text = "20";
+
+                    correctAnswer = 1;
+
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.Q3;
+
+                    labelQuestion.Text = "The simplest form of 1.5 : 2.5 is.......";
+
+                    button5.Text = "6 : 10";
+                    button6.Text = "15 : 25";
+                    button7.Text = "0.75 : 1.25";
+                    button8.Text = "3 : 5";
+
+                    correctAnswer = 4;
+
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.Q4;
+
+                    labelQuestion.Text = "The average of first 50 natural numbers is ………";
+
+                    button5.Text = "25.30";
+                    button6.Text = "25.5";
+                    button7.Text = "25.00";
+                    button8.Text = "12.25";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.Q5;
+
+                    labelQuestion.Text = "The number of 3-digit numbers divisible by 6, is …………";
+
+                    button5.Text = "149";
+                    button6.Text = "166";
+                    button7.Text = "150";
+                    button8.Text = "151";
+
+                    correctAnswer = 3;
+
+                    break;
+
+
+
+
+            }
+        }
+
+      
+    }
+}
+
